133f124dd1c868116ef76d28ad48298c0ce4a48f
poly <1037533792@qq.com>
2020-05-22 18:24:31 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,11 @@ const path = require("path");
 
 module.exports={
     mode:'development',
+    devtool: "inline-source-map",
+    devServer: {
+      contentBase: "./dist"
+    },
+
     entry:'./src/index.js',
     output:{
         filename:'index.[contenthash].js'
