79b7356b1e460e5ebe516abe2fc4899f67dd6340
poly <1037533792@qq.com>
2020-05-23 15:55:19 +0800

    准备上传了

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,16 @@
 
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require("path");
 
+const base=require('./webpack.config.base.js')
 module.exports={
-    mode:'development',
-    devtool: "inline-source-map",
-    devServer: {
-      contentBase: "./dist"
-    },
-
-    entry:'./src/index.js',
-    output:{
-        filename:'index.[contenthash].js'
-    },
-    plugins:[new HtmlWebpackPlugin({
-        title:'爱你芝芝',
-        template:'src/assets/index.html'
-    })],
+     ...base,
+     devtool: "inline-source-map",
+     devServer: {
+       contentBase: "./dist"
+     },
     module: {
         rules: [
           {
